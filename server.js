942e8600f6fa99053b34b3fb26a23b2675d868a2
Alejandro Orué <mikee1097@gmail.com>
2025-11-13 12:22:18 -0600

    131125-1222-20

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("./src"));
+app.use(express.static("./Proyecto-ProcImg"));
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
